b31e3ba3b3e88252abe44d6a90ed05401d29ed6b
codered-epidemy <efimenkov.vanya@gmail.com>
2022-04-15 10:19:42 +0500

    tcp_task_1

---
+++ main.py
@@ -0,0 +1,36 @@
+import socket
+import threading
+import time
+
+
+list_of_ports = list()
+
+
+def scan_port(ip, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.5)
+    try:
+        conn = s.connect((ip, port))
+        list_of_ports.append(port)
+        time.sleep(5)
+        s.close()
+    except:
+        pass
+
+
+try:
+    addr = input("Enter IP or domain: ")
+    address = socket.gethostbyname(addr)
+except:
+    raise IOError("Inputted value might be incorrect IP or domain")
+
+start_port, end_port = input("Enter start port and end port: ").split(" ")
+print(socket.gethostbyname(address))
+for i in range(int(start_port), int(end_port) + 1):
+    thread = threading.Thread(target=scan_port, args=(address, i))
+    thread.start()
+
+if len(list_of_ports) == 0:
+    print("No open ports")
+else:
+    print("Open ports:", *list_of_ports)
